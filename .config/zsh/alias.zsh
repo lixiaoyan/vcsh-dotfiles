437a2f245138c9df05d9e389cf39bc5eb71fc255
XiaoYan Li <lxy.lixiaoyan@gmail.com>
2018-02-05 03:53:24 +0800

    add zsh config

---
+++ .config/zsh/alias.zsh
@@ -0,0 +1,3 @@
+alias ll="ls -l --human-readable --time-style=long-iso"
+
+eval $(hub alias -s)
---
+++ .config/zsh/init.zsh
@@ -0,0 +1,38 @@
+# core
+HISTFILE=~/.zsh_history
+HISTSIZE=10000
+SAVEHIST=10000
+
+setopt INC_APPEND_HISTORY
+
+setopt AUTO_PUSHD
+
+zstyle ":completion:*" menu select
+zstyle ":completion:*" insert-tab false
+zstyle ':completion:*' special-dirs true
+
+# color schema
+source ~/.config/zsh/base16-shell/scripts/base16-onedark.sh
+
+# plugins
+source ~/.config/zsh/antigen.zsh
+
+antigen bundle mafredri/zsh-async
+
+antigen bundle sindresorhus/pure
+
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-completions
+antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle zsh-users/zsh-history-substring-search
+
+antigen apply
+
+# key bindings
+bindkey -v
+
+bindkey "^[[A" history-substring-search-up
+bindkey "^[[B" history-substring-search-down
+
+# others
+source ~/.config/zsh/alias.zsh
